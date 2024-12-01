26a489f21771f4c7690bb15a3e79e304e0eac8c7
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2024-12-01 10:45:39 +0900

    feat:api呼び出し確認完了
    jrのリアルタイム車両位置データの取得

--- 02_プロトタイプ/test-app/src/infrastructure/axios/railway/reqRailwayData.ts
+++ 02_プロトタイプ/test-app/src/infrastructure/axios/railway/reqRailwayData.ts
@@ -7,12 +7,18 @@ const httpLimited = getInstanceLimited()
 const useReqRailwayData = () => {
   //jr東日本の車両位置リアルタイムデータ
   const reqJrEastRealTimeData = async () => {
-    const config = {
-      method: "GET",
-      url: `gtfs/realtime/jreast_odpt_train_vehicle?acl:consumerKey=[${process.env.NEXT_PUBLIC_OPEN_DATA_CHALLENGE_TOKEN_LIMITED}]`,
+    try{
+      const token = process.env.NEXT_PUBLIC_OPEN_DATA_CHALLENGE_TOKEN_LIMITED
+      const config = {
+        method: "GET",
+        url: `gtfs/realtime/jreast_odpt_train_vehicle?acl:consumerKey=${token}`,
+      }
+      const res = await httpLimited.request(config)
+      console.log(res)
+    }catch(e:any){
+      console.log(e)
     }
-    const res = await httpLimited.request(config)
-    console.log(res)
+   
   }
   return {
     reqJrEastRealTimeData,
