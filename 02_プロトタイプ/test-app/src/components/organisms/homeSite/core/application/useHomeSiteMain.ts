a517f20b66b40427fb1eef32aaa9e3cd4eb28a83
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2024-12-01 18:49:26 +0900

    feat:homeSiteの修正

--- 02_プロトタイプ/test-app/src/components/organisms/homeSite/core/application/useHomeSiteMain.ts
+++ 02_プロトタイプ/test-app/src/components/organisms/homeSite/core/application/useHomeSiteMain.ts
@@ -5,11 +5,22 @@ const useHomeSiteMain = () => {
    * ボタンがクリックされた場合
    **/
   const buttonClicked = async () => {
+    alert("この関数はorganisms/homeSite/core/application/useHomeSiteMain.tsにあるよ！")
+  }
+  const jrEastRealTimeLocateDataCallback = async () => {
     //JR東日本リアルタイム車両位置データを非同期で取得する
     await reqJrEastRealTimeLocateData()
   }
+
+  // メニューページに遷移する関数
+  const navigateToMenuSite = () => {
+    window.location.href = "/menu" // メニューページに遷移する
+  }
+
   return {
     buttonClicked,
+    jrEastRealTimeLocateDataCallback,
+    navigateToMenuSite,
   }
 }
 export default useHomeSiteMain
--- 02_プロトタイプ/test-app/src/components/organisms/homeSite/ui/HomeSiteMain.tsx
+++ 02_プロトタイプ/test-app/src/components/organisms/homeSite/ui/HomeSiteMain.tsx
@@ -1,19 +1,43 @@
 "use client"
-import Link from "next/link"
 import React from "react"
-import SearchBar from "@/components/molecules/forms/SearchBar"
 import Header from "@/components/molecules/header/Header"
 import Button from "@/components/atoms/buttons/Button"
 import TextLabel from "@/components/atoms/labels/TextLabel"
 import useHomeSiteMain from "@/components/organisms/homeSite/core/application/useHomeSiteMain"
 
 const HomeSiteMain: React.FC = () => {
-  const { buttonClicked } = useHomeSiteMain()
-
+  const { buttonClicked, navigateToMenuSite, jrEastRealTimeLocateDataCallback } = useHomeSiteMain()
   return (
     <div>
-      <Header icon="menu" size="normal" variant="header-primary" onClick={buttonClicked} />
-      <Button icon="refresh" variant="btn-primary" size="small" onClick={buttonClicked} />
+      <Header
+        text="API取得テストホームページ　"
+        icon="menu"
+        size="normal"
+        variant="header-primary"
+        onClick={buttonClicked}
+      />
+      <div className="m-4">
+        <div className="flex justify-center items-center gap-4">
+          <TextLabel text="JR東日本リアルタイム車両位置データ" size="normal" bold={false} />
+          <Button
+            text="更新する"
+            icon="refresh"
+            variant="btn-primary"
+            size="small"
+            onClick={jrEastRealTimeLocateDataCallback}
+          />
+        </div>
+        <div className="flex justify-center my-4">
+          {/* メニューページに遷移するボタン */}
+          <Button
+            text="メニューページに遷移する"
+            variant="btn-warning"
+            size="normal"
+            shape="circle"
+            onClick={navigateToMenuSite}
+          />
+        </div>
+      </div>
     </div>
   )
 }
