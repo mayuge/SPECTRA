620ebee13f3f2a3fe05f32c055e4f84f84ee4627
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2024-12-03 23:59:39 +0900

    feat:assetフォルダ作成

--- 02_プロトタイプ/test-app/src/components/organisms/menuSite/ui/MenuSiteMain.tsx
+++ 02_プロトタイプ/test-app/src/components/organisms/menuSite/ui/MenuSiteMain.tsx
@@ -10,39 +10,46 @@ const MenuSiteMain: React.FC = () => {
   const { buttonClicked, navigateToHomeSite, getCount } = useMenuSiteMain()
 
   return (
-    <div>
-      <div className="relative z-10 p-4 flex justify-center">
-        <div className="max-w-screen-xl w-full mx-auto mx-8">
-          <Header
-            text="NOZAWA OPENDATA PROJECT"
-            icon="menu"
-            size="large"
-            variant="header-light"
-            isCircle={true}
-            onClick={buttonClicked}
-          />
-        </div>
-      </div>
-
-      <div className="m-4">
-        <div>
-          <TextLabel
-            text={`状態管理ツールテスト用カウンター:${getCount()}`}
-            size="normal"
-            bold={false}
-          />
-        </div>
-        <div className="flex justify-center my-8">
-          <Button
-            text="ホームページに遷移する"
-            variant="btn-warning"
-            size="normal"
-            shape="circle"
-            onClick={navigateToHomeSite}
-          />
-        </div>
+    <div className="relative">
+    <img
+      src="/assets/platou_test2.jpeg"
+      className="absolute inset-0 w-full h-full object-cover z-[-1]"
+    />
+    
+    <div className="relative z-10 p-4 flex justify-center">
+      <div className="max-w-screen-xl w-full mx-auto mx-8">
+        <Header
+          text="NOZAWA OPENDATA PROJECT"
+          icon="menu"
+          size="large"
+          variant="header-light"
+          isCircle={true}
+          onClick={buttonClicked}
+        />
       </div>
     </div>
+  
+    <div className="m-4">
+      <div>
+        <TextLabel
+          text={`状態管理ツールテスト用カウンター:${getCount()}`}
+          size="normal"
+          bold={false}
+        />
+      </div>
+      <div className="flex justify-center my-8">
+        <Button
+          text="ホームページに遷移する"
+          iconRight="arrow_forward"
+          variant="btn-dark"
+          size="normal"
+          shape="circle"
+          onClick={navigateToHomeSite}
+        />
+      </div>
+    </div>
+  </div>
+  
   )
 }
 
