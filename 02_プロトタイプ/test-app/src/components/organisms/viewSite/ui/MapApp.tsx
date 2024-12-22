d7827101ba543a07c0c1faaec1be52725ceea743
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2024-12-23 00:24:50 +0900

    feat:マップ修正

--- 02_プロトタイプ/test-app/src/components/organisms/viewSite/ui/MapApp.tsx
+++ 02_プロトタイプ/test-app/src/components/organisms/viewSite/ui/MapApp.tsx
@@ -1,9 +1,7 @@
 "use client"
-
 import React, { useRef, useEffect } from "react"
 import maplibregl, { RasterSourceSpecification, GeoJSONSourceSpecification, PropertyValueSpecification, LayerSpecification } from "maplibre-gl"
 import "maplibre-gl/dist/maplibre-gl.css"
-import { mapConfig } from "@/components/organisms/viewSite/core/params/params"
 
 // レイヤーの型定義
 type Layer = {
@@ -18,95 +16,94 @@ type Layer = {
   }
   paint: any
 }
+// レイヤー定義オブジェクト（複数レイヤに対応）
+const layers: Layer[] = [
+  {
+    id: "konjakumap",
+    type: "raster",
+    sourceId: "konjakumap",
+    source: {
+      type: "raster",
+      tiles: ["https://ktgis.net/kjmapw/kjtilemap/tokyo50/03/{z}/{x}/{y}.png"],
+      tileSize: 512,
+      scheme: "tms",
+      maxzoom: 16,
+      minzoom: 8,
+      attribution: "今昔マップ",
+    } as RasterSourceSpecification,
+    layout: {
+      visibility: "visible",
+    },
+    paint: {
+      "raster-opacity": 0.5,
+    },
+  },
+  {
+    id: "osm-map",
+    type: "raster",
+    sourceId: "osm-map",
+    source: {
+      type: "raster",
+      tiles: ["https://tile.openstreetmap.jp/styles/osm-bright-ja/{z}/{x}/{y}.png"],
+      tileSize: 512,
+      attribution: "OpenStreetMap",
+    } as RasterSourceSpecification,
+    layout: {
+      visibility: "visible",
+    },
+    paint: {
+      "raster-opacity": 0.5,
+    },
+  },
+  {
+    id: "polygon",
+    type: "fill",
+    sourceId: "BaseCityBlocks",
+    source: {
+      type: "geojson",
+      data: "/geojson/Block_Tokyo_FeaturesToJSON.geojson",
+    } as GeoJSONSourceSpecification,
+    layout: {
+      visibility: "visible",
+    },
+    paint: {
+      "fill-color": [
+        "interpolate",
+        ["linear"],
+        ["get", "Capacity_面積"],
+        0, "#f2f0f7",
+        10, "#cbc9e2",
+        20, "#9e9ac8",
+        50, "#6a51a3"
+      ],
+      "fill-opacity": 0.7,
+    },
+  },
+  {
+    id: "line",
+    type: "line",
+    sourceId: "LineToeiBus",
+    source: {
+      type: "geojson",
+      data: "/geojson/LineToeiBus.geojson",
+    } as GeoJSONSourceSpecification,
+    layout: {
+      "line-join": "round",
+      "line-cap": "round",
+      visibility: "visible",
+    },
+    paint: {
+      "line-color": "#00FF00",
+      "line-width": 3,
+      "line-opacity": 0.8,
+    },
+  },
+]
 
 const MapApp: React.FC = () => {
   const mapContainer = useRef<HTMLDivElement>(null)
   const mapInstance = useRef<maplibregl.Map | null>(null)
-
-  // レイヤー定義オブジェクト（複数レイヤに対応）
-  const layers: Layer[] = [
-    {
-      id: "konjakumap",
-      type: "raster",
-      sourceId: "konjakumap",
-      source: {
-        type: "raster",
-        tiles: ["https://ktgis.net/kjmapw/kjtilemap/tokyo50/03/{z}/{x}/{y}.png"],
-        tileSize: 512,
-        scheme: "tms",
-        maxzoom: 16,
-        minzoom: 8,
-        attribution: "今昔マップ",
-      } as RasterSourceSpecification,
-      layout: {
-        visibility: "visible",
-      },
-      paint: {
-        "raster-opacity": 0.5,
-      },
-    },
-    {
-      id: "osm-map",
-      type: "raster",
-      sourceId: "osm-map",
-      source: {
-        type: "raster",
-        tiles: ["https://tile.openstreetmap.jp/styles/osm-bright-ja/{z}/{x}/{y}.png"],
-        tileSize: 512,
-        attribution: "OpenStreetMap",
-      } as RasterSourceSpecification,
-      layout: {
-        visibility: "visible",
-      },
-      paint: {
-        "raster-opacity": 0.5,
-      },
-    },
-    {
-      id: "polygon",
-      type: "fill",
-      sourceId: "BaseCityBlocks",
-      source: {
-        type: "geojson",
-        data: "/geojson/Block_Tokyo_FeaturesToJSON.geojson",
-      } as GeoJSONSourceSpecification,
-      layout: {
-        visibility: "visible",
-      },
-      paint: {
-        "fill-color": [
-          "interpolate",
-          ["linear"],
-          ["get", "Capacity_面積"],
-          0, "#f2f0f7",
-          10, "#cbc9e2",
-          20, "#9e9ac8",
-          50, "#6a51a3"
-        ],
-        "fill-opacity": 0.7,
-      },
-    },
-    {
-      id: "line",
-      type: "line",
-      sourceId: "LineToeiBus",
-      source: {
-        type: "geojson",
-        data: "/geojson/LineToeiBus.geojson",
-      } as GeoJSONSourceSpecification,
-      layout: {
-        "line-join": "round",
-        "line-cap": "round",
-        visibility: "visible",
-      },
-      paint: {
-        "line-color": "#00FF00",
-        "line-width": 3,
-        "line-opacity": 0.8,
-      },
-    },
-  ]
-
+  
   // displayMap関数内は空の箱のようにしておき、url等もあとから中身を追加する
   const displayMap = () => {
     if (mapContainer.current && !mapInstance.current) {
