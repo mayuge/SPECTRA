be872b8493d33b3896690e70048b1979c1565f3b
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2024-12-23 22:48:28 +0900

    feat:レイヤーの合成

--- 02_src/NozawaOpenDataProject/src/components/organisms/viewSite/core/params/layers.ts
+++ 02_src/NozawaOpenDataProject/src/components/organisms/viewSite/core/params/layers.ts
@@ -1,5 +1,6 @@
 import { CardListType } from "@/domain/types/cardListType"
-
+import { LayerType } from "@/domain/types/layerType"
+import  { RasterSourceSpecification, GeoJSONSourceSpecification } from "maplibre-gl"
 export const cardList: CardListType[] = [
   {
     logoImg: "/assets/logos/jreast.webp",
@@ -48,5 +49,88 @@ export const cardList: CardListType[] = [
     infoButtonClick: "openAllDialogs",
     displayButtonClick: "buttonClicked",
     orderButtonClick: "buttonClicked",
+  },
+]
+// レイヤー定義オブジェクト（複数レイヤに対応）
+export const layerObjects:  LayerType[] = [
+  {
+    id: "konjakumap",
+    type: "raster",
+    sourceId: "konjakumap",
+    source: {
+      type: "raster",
+      tiles: ["https://ktgis.net/kjmapw/kjtilemap/tokyo50/03/{z}/{x}/{y}.png"],
+      tileSize: 512,
+      scheme: "tms",
+      maxzoom: 16,
+      minzoom: 8,
+      attribution: "今昔マップ",
+    } as RasterSourceSpecification,
+    layout: {
+      visibility: "visible",
+    },
+    paint: {
+      "raster-opacity": 1,
+    },
+  },
+  {
+    id: "osm-map",
+    type: "raster",
+    sourceId: "osm-map",
+    source: {
+      type: "raster",
+      tiles: ["https://tile.openstreetmap.jp/styles/osm-bright-ja/{z}/{x}/{y}.png"],
+      tileSize: 512,
+      attribution: "OpenStreetMap",
+    } as RasterSourceSpecification,
+    layout: {
+      visibility: "visible",
+    },
+    paint: {
+      "raster-opacity": 1,
+    },
+  },
+  {
+    id: "polygon",
+    type: "fill",
+    sourceId: "BaseCityBlocks",
+    source: {
+      type: "geojson",
+      data: "/geojson/Block_Tokyo_FeaturesToJSON.geojson",
+    } as GeoJSONSourceSpecification,
+    layout: {
+      visibility: "visible",
+    },
+    paint: {
+      "fill-color": [
+        "interpolate",
+        ["linear"],
+        ["get", "Capacity_面積"],
+        0, "#f2f0f7",
+        10, "#cbc9e2",
+        20, "#9e9ac8",
+        50, "#6a51a3"
+      ],
+      "fill-opacity": 0.7,
+    },
+  },
+  {
+    id: "line",
+    type: "line",
+    sourceId: "LineToeiBus",
+    source: {
+      type: "geojson",
+      data: "/geojson/LineToeiBus.geojson",
+    } as GeoJSONSourceSpecification,
+    layout: {
+      "line-join": "round",
+      "line-cap": "round",
+      visibility: "visible",
+    },
+    paint: {
+      "line-color": "#00FF00",
+      "line-width": 3,
+      "line-opacity": 0.8,
+    },
   },
 ]
