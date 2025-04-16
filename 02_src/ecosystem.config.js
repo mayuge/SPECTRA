7c3bea5ff8a1e9976864dcf0a7f05b98f573ed2f
se-ueda <sei2000u.27@gmail.com>
2025-04-17 00:54:56 +0900

    feat:サーバーbuild対応

--- 02_src/ecosystem.config.js
+++ 02_src/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: 'test-app',
         script: 'node_modules/next/dist/bin/next',
-        args: 'dev -p 3000', // 本番運用時は、startに変更
+        args: 'start -p 3000',
         cwd: "./NozawaOpenDataProject",
         instances: 1,
         watch: false,
