60b1469f0c1be82ce98802c5bf951aaf96d0f2af
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2025-08-02 21:25:30 +0900

    feat:fastmcpの依存関係を変更

--- 03_プロトタイプ/postgis/backend/infrastructure/mcp/chat_repository.py
+++ 03_プロトタイプ/postgis/backend/infrastructure/mcp/chat_repository.py
@@ -1,20 +1,20 @@
-from fastmcp import FastMCP
+from mcp.server.fastmcp import FastMCP
 import httpx
 from infrastructure.database.train.train_repository import TrainRepository
 
 mcp = FastMCP("chat-server")
 train_repository = TrainRepository()
 
+http_instance = httpx.AsyncClient()
 
 @mcp.tool()
 async def get_station_by_name(station_name: str):
     """駅名を指定して駅情報を取得します。最後の文字が駅だった場合は引数に入れない。引数の例: '東京', '池袋'"""
     #http://localhost:4000/train/station/東京
-    async with httpx.AsyncClient() as client:
-        url = f"http://localhost:4000/train/station/{station_name}"
-        response = await client.get(url)
-        response.raise_for_status()
-        return response.json()
+    url = f"http://localhost:4000/train/station/{station_name}"
+    response = await http_instance.get(url)
+    response.raise_for_status()
+    return response.json()
 
 
 
@@ -22,11 +22,10 @@ async def get_station_by_name(station_name: str):
 async def get_train_line_by_name(line_name: str):
     """路線名を指定して路線情報を取得します。引数の例: '池袋線', '山手線'"""
     #http://localhost:4000/train/line/池袋線
-    async with httpx.AsyncClient() as client:
-        url = f"http://localhost:4000/train/line/{line_name}"
-        response = await client.get(url)
-        response.raise_for_status()
-        return response.json()
+    url = f"http://localhost:4000/train/line/{line_name}"
+    response = await http_instance.get(url)
+    response.raise_for_status()
+    return response.json()
 
 
 if __name__ == "__main__":
