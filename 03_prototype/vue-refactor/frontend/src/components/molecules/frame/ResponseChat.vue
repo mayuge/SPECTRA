9252505ccded74efb058d8a72be3c94c2d375cdf
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2025-11-05 00:36:24 +0900

    feat:トグル機能追加

--- 03_prototype/vue-refactor/frontend/src/components/molecules/frame/ResponseChat.vue
+++ 03_prototype/vue-refactor/frontend/src/components/molecules/frame/ResponseChat.vue
@@ -1,16 +1,41 @@
 <template>
-  <div class="flex justify-start w-full p-2">
-    <div class="bg-gray-80 flex justify-center text-black rounded-md px-2 py-4 text-xs w-[50%] ">
+  <div class="flex flex-col justify-start w-full p-2 text-xs">
+    <div class="bg-gray-80 flex justify-center text-black rounded-t-md px-2 py-4  w-[50%] ">
       {{ text }}
+    </div>
+    <div
+      class="flex items-center bg-gray-20 w-[50%] rounded-b-md px-2 gap-2 text-white"
+      @click="toggleClicked()"
+    >
+      <Button
+        size="mini"
+        :icon-left="iconState ? 'visibility': 'visibility_off'"
+        variant="btn-text-white"
+      />
+      <div>表示切替</div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import type { PropType } from "vue"
+import  { ref, type PropType } from "vue"
+import Button from "@/components/atoms/buttons/Button.vue"
 
-defineProps({
+const iconState = ref(true)
+
+const emit = defineEmits(["toggle-clicked"])
+
+const toggleClicked = () => {
+    iconState.value = !iconState.value
+    emit("toggle-clicked", props.responseId)
+}
+
+const props = defineProps({
     text : {
         type: String as PropType<string>,
-    }
+    },
+    responseId : {
+        type:Number as PropType<number>,
+        required:true
+    },
 })
 </script>
